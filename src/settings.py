0720a835ceeb8d90a8360540091079176a02d208
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-10 20:16:39 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Rice Disease"
 PROJECT_NAME_FULL: str = "Rice Leaf Diseases with Boundary Box"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -83,6 +84,7 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
